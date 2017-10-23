91cc658774f61ca6caa91a500fb046d3485bd1e3
mssavai <mssavai@gmail.com>
2017-10-23 15:58:13 +0300

    MUZIMA-536: Settings & more updates

---
+++ api/src/main/java/org/openmrs/module/muzima/utils/Constants.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.muzima.utils;
+
+public class Constants {
+    public static final class MuzimaSettings{
+        public static final String PATIENT_IDENTIFIER_AUTOGENERATTION_SETTING_PROPERTY = "PatientIdentifier.AutoGeneration";
+        public static final String PATIENT_IDENTIFIER_AUTOGENERATTION_SOURCE_NAME
+                = "PatientIdentifier.AutoGenerationSourceName";
+        public static final Boolean PATIENT_IDENTIFIER_AUTOGENERATTION_SETTING_DEFAULT_VALUE = false;
+    }
+}
