01ec9c6e6fff636a77108450dbf7d4f2c9049f30
JavaDeveloper <samuelowino43@yahoo.com>
2017-12-17 19:41:01 +0300

    Refactored TestContextConfiguration facility for testing db bean and model objects

--- api/src/test/java/org/openmrs/module/muzima/api/db/hibernate/HibernateRegistrationDataDaoTest.java
+++ api/src/test/java/org/openmrs/module/muzima/api/db/hibernate/HibernateRegistrationDataDaoTest.java
@@ -1,41 +1,59 @@
 package org.openmrs.module.muzima.api.db.hibernate;
 
+import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
 import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.openmrs.module.muzima.api.db.RegistrationDataDao;
+import org.openmrs.module.muzima.testContexts.DaoTestContextsConfigurations;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
-@ContextConfiguration
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class HibernateRegistrationDataDaoTest {
 
 
+    private HibernateRegistrationDataDao hibernateRegistrationDao;
+
+    private SessionFactory dbSessionFactory;
+
+    @InjectMocks
+    private RegistrationDataDao registrationDataDao;
 
     @Before
     public void setUp() throws Exception {
-        AnnotationConfigApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext("org.openmrs.module.muzima.api");
-        applicationContext.getBean(DbSessionFactory.class);
-    }
-
-    @After
-    public void tearDown() throws Exception {
+        ApplicationContext testApplicationContext =
+                new AnnotationConfigApplicationContext(DaoTestContextsConfigurations.class);
+        this.hibernateRegistrationDao = testApplicationContext.getBean(HibernateRegistrationDataDao.class);
+        this.dbSessionFactory = testApplicationContext.getBean(SessionFactory.class);
     }
 
     @Test
-    public void getSessionFactory() throws Exception {
+    public void creationTest() throws Exception {
+      assertThat(hibernateRegistrationDao).isNotNull();
+      assertThat(hibernateRegistrationDao).isInstanceOf(HibernateRegistrationDataDao.class);
+      assertThat(dbSessionFactory).isNotNull();
+      assertThat(dbSessionFactory).isInstanceOf(DbSessionFactory.class);
     }
 
     @Test
-    public void getRegistrationDataById() throws Exception {
+    public void getSessionFactoryTest() throws Exception {
+
     }
 
     @Test
-    public void getRegistrationDataByUuid() throws Exception {
+    public void getRegistrationDataByIdTest() throws Exception {
     }
 
     @Test
-    public void getRegistrationData() throws Exception {
+    public void getRegistrationDataByUuidTest() throws Exception {
+    }
+
+    @Test
+    public void getRegistrationDataTest() throws Exception {
     }
 
     @Test
@@ -43,15 +61,20 @@ public class HibernateRegistrationDataDaoTest {
     }
 
     @Test
-    public void deleteRegistrationData() throws Exception {
+    public void deleteRegistrationDataTest() throws Exception {
     }
 
     @Test
-    public void getRegistrationData1() throws Exception {
+    public void getRegistrationData1Test() throws Exception {
     }
 
     @Test
-    public void countRegistrationData() throws Exception {
+    public void countRegistrationDataTest() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dbSessionFactory.getCurrentSession().close();
     }
 
 }
