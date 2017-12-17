4c659336e278cbb2681256d4b43bd6a11ad133ba
JavaDeveloper <samuelowino43@yahoo.com>
2017-12-17 05:29:47 +0300

    Completely refactored JsonUtilsTest case methods, 12 of 15 tests pased on the run done at the time of this commit, one method  writeAsBoolean required production code refatoring, the remaing two tests writeAsDateTime and ReadAsDateTime require ISO80U..Date scrutiny

--- api/src/test/java/org/openmrs/module/muzima/handler/JsonRegistrationQueueDataHandlerTest.java
+++ api/src/test/java/org/openmrs/module/muzima/handler/JsonRegistrationQueueDataHandlerTest.java
@@ -3,17 +3,11 @@ package org.openmrs.module.muzima.handler;
 import org.assertj.core.api.Assertions;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.openmrs.Patient;
 import org.openmrs.module.muzima.model.QueueData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 
 @ContextConfiguration(locations = "/TestingApplicationContext.xml")
 public class JsonRegistrationQueueDataHandlerTest {
@@ -36,7 +30,6 @@ public class JsonRegistrationQueueDataHandlerTest {
         Assertions.assertThat(jsonRegistrationQueueDataHandler.getDiscriminator()).isNotEmpty();
         Assertions.assertThat(jsonRegistrationQueueDataHandler.getDiscriminator()).isEqualTo("json-registration");
     }
-
 
 
 }
