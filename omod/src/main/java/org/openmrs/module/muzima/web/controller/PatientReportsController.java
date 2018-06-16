826af81f7e21409bd15f25855f49442781832228
dilekamadushan <dileka.14@cse.mrt.ac.lk>
2018-06-16 16:17:53 +0530

    added first changes

---
+++ omod/src/main/java/org/openmrs/module/muzima/web/controller/PatientReportsController.java
@@ -0,0 +1,56 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.muzima.web.controller;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.muzima.api.service.DataService;
+import org.openmrs.module.muzima.model.DataSource;
+import org.openmrs.module.muzima.web.utils.WebConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * TODO: Write brief description about the class here.
+ */
+@Controller
+@RequestMapping(value = "/module/muzimacore/patientReport.json")
+public class PatientReportsController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public Map<String, Object> getSources(final @RequestParam(value = "search") String search,
+                                          final @RequestParam(value = "pageNumber") Integer pageNumber,
+                                          final @RequestParam(value = "pageSize") Integer pageSize) {
+        Map<String, Object> response = new HashMap<String, Object>();
+        if (Context.isAuthenticated()) {
+            DataService dataService = Context.getService(DataService.class);
+            int pages = (dataService.countDataSource(search).intValue() + pageSize - 1) / pageSize;
+            List<Object> objects = new ArrayList<Object>();
+            for (DataSource dataSource : dataService.getPagedDataSource(search, pageNumber, pageSize)) {
+                objects.add(WebConverter.convertDataSource(dataSource));
+            }
+            response.put("pages", pages);
+            response.put("objects", objects);
+        }
+        return response;
+    }
+}
