04250636fc3ad505d99fbf8a7e81dc74eb48a1db
mssavai <mssavai@gmail.com>
2018-09-14 09:41:23 +0300

    Remove unnecessary files, rename user messages

--- build.sh
+++
@@ -1,7 +0,0 @@
-mvn clean package -DskipTests
-echo 'Deleting installed module .... ....'
-sudo rm /var/lib/OpenMRS/modules/muzimacore-1.2.0.omod
-echo 'Deploying new module ... ...'
-sudo cp omod/target/muzimacore-1.2.0.omod /var/lib/OpenMRS/modules/
-echo 'Restarting tomcat ... ...'
-sudo service tomcat7 restart
