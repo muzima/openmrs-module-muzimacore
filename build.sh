a20b7725cf9a3a21426d7ff79304c0a1b78400b8
benamonya <benanyanga@gmail.com>
2018-11-28 18:40:42 +0300

    FGH-15: Deleted build files

--- build.sh
+++
@@ -1,9 +0,0 @@
-mvn clean package -DskipTests
-echo 'Deleting installed module .... ....'
-sudo rm /var/lib/OpenMRS/modules/muzimacore-1.2.0.omod
-echo 'Deploying new module ... ...'
-sudo cp omod/target/muzimacore-1.2.0.omod /var/lib/OpenMRS/modules/
-echo 'Restarting tomcat ... ...'
-sudo service tomcat7 restart
-sudo tail -f /var/log/tomcat7/catalina.out
-
