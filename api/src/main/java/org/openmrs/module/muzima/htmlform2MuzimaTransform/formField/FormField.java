c9a768cec549426f9151e0e5ec0793622f471580
Aishat-Akinyemi <aishatadenike01@gmail.com>
2020-01-21 11:34:25 +0100

    creating FormField interface for Fields to implement

---
+++ api/src/main/java/org/openmrs/module/muzima/htmlform2MuzimaTransform/formField/FormField.java
@@ -0,0 +1,17 @@
+package org.openmrs.module.muzima.htmlform2MuzimaTransform.formField;
+
+/*
+ * This represents a field on a form such as text, textarea, number, select, radio etc.
+ *  
+ */
+public interface FormField {
+	
+	/**
+	 * called to generate html5 code corresponding to an muzima tag
+	 * 
+	 * @return html code as String
+	 */
+	public String generateHtml();
+	
+	public void setDefaultValue(Object defaultValue);
+}
