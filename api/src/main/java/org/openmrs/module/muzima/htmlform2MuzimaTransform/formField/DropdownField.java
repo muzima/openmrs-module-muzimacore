7a294b50b9e5351dc4ad4d4ddbf0486e27f80365
Aishat-Akinyemi <aishatadenike01@gmail.com>
2020-01-21 16:57:18 +0100

    Create DropdownField.java
    Generates code for a dropdown (select) form field like
     `<select name="..."><option value="...">...</option></select>`

---
+++ api/src/main/java/org/openmrs/module/muzima/htmlform2MuzimaTransform/formField/DropdownField.java
@@ -0,0 +1,72 @@
+package org.openmrs.module.muzima.htmlform2MuzimaTransform.formField;
+
+import java.util.Locale;
+
+import org.openmrs.Concept;
+
+/**
+ * Generates code for a dropdown (select) form field like
+ * {@code <select name="..."><option value="...">...</option></select>}
+ */
+public class DropdownField extends SingleOptionField {
+	
+	private String fieldLabel;
+	
+	private String name;
+	
+	private String dataConcept;
+	
+	private boolean required = false;
+	
+	private Integer size;
+	
+	public DropdownField() {
+	}
+	
+	public DropdownField(Concept concept, Locale locale, String label, Integer size) {
+		this.size = size;
+		this.name = FieldFactory.createNameAttributeFromConcept(concept, locale);
+		this.dataConcept = FieldFactory.createDataConceptAttributeFromConcept(concept, locale);
+		this.fieldLabel = label;
+	}
+	
+	@Override
+	public String generateHtml() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("<div class=\"form-group\">\r\n" + "            <label for=\"" + this.name + "\">" + this.fieldLabel);
+		if (this.required) {
+			sb.append(" <span class=\"required\">*</span>");
+		}
+		sb.append("</label>");
+		sb.append("<select id=\"" + this.name + "\" name=\"" + this.name + "\"");
+		if (this.required) {
+			sb.append(" required =\"required \" ");
+		}
+		sb.append(" data-concept=" + this.dataConcept);
+		if (size != null) {
+			if (size == 999) {
+				size = getOptions().size() + 1; // Add one to make sure all elements show up without scrollbar
+			}
+			sb.append(" size=").append("\"" + size.intValue() + "\"");
+		}
+		sb.append(">");
+		
+		for (int i = 0; i < getOptions().size(); ++i) {
+			Option option = getOptions().get(i);
+			boolean selected = option.isSelected();
+			if (!selected)
+				selected = getDefaultValue() == null ? option.getValue().equals("")
+				        : getDefaultValue().equals(option.getValue());
+			sb.append("<option value=\"").append(option.getValue()).append("\"");
+			if (selected)
+				sb.append(" selected=\"true\"");
+			sb.append(">");
+			sb.append(option.getLabel());
+			sb.append("</option>");
+		}
+		sb.append("</select>");
+		
+		return null;
+	}
+	
+}
