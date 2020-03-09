c430aa5d7260ac44c16c2acc4a3833a2b731b742
Aishat-Akinyemi <52966744+Aishat-Akinyemi@users.noreply.github.com>
2020-03-09 12:15:28 +0100

    create and consume saveConvertedForm() service
    Add ability to save the output muzima forms into the muzimaforms table
    * created the saveConvertedForm() in MuzimaFormService
    * add saveConvertedForm to  MuzimaHtmlFormEntryController
    *  modified $ctrl.save in HtmlFormEntryController.js to be used in app.js
    *

--- omod/src/main/java/org/openmrs/module/muzima/web/controller/MuzimaHtmlFormEntryController.java
+++ omod/src/main/java/org/openmrs/module/muzima/web/controller/MuzimaHtmlFormEntryController.java
@@ -14,6 +14,7 @@ import org.openmrs.module.htmlformentry.HtmlFormEntryService;
 import org.openmrs.module.muzima.api.service.MuzimaFormService;
 import org.openmrs.module.muzima.web.utils.WebConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -36,39 +37,38 @@ public class MuzimaHtmlFormEntryController {
                     objects.add(WebConverter.convertHtmlForm(form));
                 }
             }
-
             response.put("objects", objects);
         }
-
         return response;
     }
 
     @ResponseBody
     @RequestMapping(value = "module/muzimacore/htmlFormEntry.form", method = RequestMethod.POST)
-    public Map<String, Object> convertHtmlForms(final @RequestParam(value = "id") Integer id) {
-        Map<String, Object> response = new HashMap<String, Object>();
-        // some dummy values for the dummy response
-		String uuid; //= "e72c4bd8-6329-4eb4-b0bd-1c4729ada98e";
-		String formId;// = "10";
-		String name;// = "mUzima Registration Form";
+	public Map<String, String> convertHtmlForms(final @RequestParam(value = "id") Integer id) {
+		Map<String, String> response = new HashMap<String, String>();
+		String uuid;
+		String formId;
+		String name;
 		String discriminator = "json-encounter";
-		String description;// = "mUzima Registration Form for testing purposes";
+		String description;
 		String html;
 		
         if (Context.isAuthenticated()) {
-
 			
 			HtmlFormEntryService htmlFormEntryService = Context.getService(HtmlFormEntryService.class);
-			Form form = Context.getService(FormService.class).getForm(id);
+			FormService formService = Context.getFormService();
+			
+			Form form = formService.getForm(id);
 			HtmlForm htmlForm = htmlFormEntryService.getHtmlFormByForm(form);
 			String htmlFormXmlString = htmlForm.getXmlData();
 			formId = id.toString();
-			uuid = htmlForm.getUuid();
-			name = htmlForm.getName();
-			description = htmlForm.getDescription();
+			//this is now Form UUID, name and description
+			uuid = form.getUuid();
+			name = form.getName();
+			description = form.getDescription();
 			
-			MuzimaFormService formService = Context.getService(MuzimaFormService.class);
-			html = formService.convertHtmlformToMuzima(htmlFormXmlString);
+			MuzimaFormService muzimaFormService = Context.getService(MuzimaFormService.class);
+			html = muzimaFormService.convertHtmlformToMuzima(htmlFormXmlString, name);
 			
 			response.put("uuid", uuid);
 			response.put("id", formId);
@@ -81,8 +81,22 @@ public class MuzimaHtmlFormEntryController {
 
         return response;
     }
+	
+	@ResponseBody
+	@RequestMapping(value = "module/muzimacore/htmlFormEntrySaveConvertedForm.form", method = RequestMethod.POST)
+	public void saveConvertedForm(final @RequestBody Map<String, Object> data) throws Exception {
+		if (Context.isAuthenticated()) {
+			String formUUID = (String) data.get("uuid");
+			String discriminator = (String) data.get("discriminator");
+			String html = (String) data.get("html");
+			MuzimaFormService muzimaFormService = Context.getService(MuzimaFormService.class);
+			muzimaFormService.saveConvertedForm(html, formUUID, discriminator);
+		}
+	}
+	
+	
 
-    @ResponseBody
+	@ResponseBody
     @RequestMapping(value = "module/muzimacore/htmlFormEntryModuleStatus.form", method = RequestMethod.GET)
     public boolean htmlFormEntryModuleStatus() {
         return ModuleFactory.isModuleStarted("htmlformentry");
