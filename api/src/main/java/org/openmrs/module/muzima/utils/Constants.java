7d8582b09ae0880955920c93d60f70d1e026f188
benamonya <benanyanga@gmail.com>
2020-05-28 12:27:26 +0300

    MUZIMA-854: Added mapping of encounters to visits

--- api/src/main/java/org/openmrs/module/muzima/utils/Constants.java
+++ api/src/main/java/org/openmrs/module/muzima/utils/Constants.java
@@ -7,5 +7,6 @@ public class Constants {
                 = "PatientIdentifier.AutoGenerationSourceName";
         public static final String MAXIMUM_ENCOUNTERS_DOWNLOAD_SETTING_PROPERTY = "Encounter.maxDownloadSize";
         public static final Boolean PATIENT_IDENTIFIER_AUTOGENERATTION_SETTING_DEFAULT_VALUE = false;
+        public static final String DEFAULT_MUZIMA_VISIT_TYPE_SETTING_PROPERTY = "mUzima.DefaultVisitType";
     }
 }
